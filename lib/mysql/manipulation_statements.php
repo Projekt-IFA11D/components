f199f7abdcfaec730b6bb53b41636eca944212f3
kpetsch <randombaur@gmail.com>
2014-05-12 12:30:20 +0200

    Check if form data columns are the same in database

--- lib/mysql/manipulation_statements.php
+++ lib/mysql/manipulation_statements.php
@@ -8,10 +8,27 @@ function read_column_names($Data) {
   
   $Table_Columns = array();
   foreach ($Data as $columns => $values) {
-    $Table_split=split("_", $columns);
+    $Table_split=split("_", $columns, 2);
     $Table_Columns[$Table_split[0]][$Table_split[1]] = $values;
   }
+  var_dump($Table_Columns);
   return $Table_Columns;
+}
+
+// Check if all columns of the form data exist in the database
+function check_column_names($Data) {
+  
+  foreach ($Data as $table => $columns) {
+    $real_cols_q=mysql_query("show columns from $table");
+    while($tmp=mysql_fetch_row($real_cols_q)) {
+      $real_cols[]=$tmp[0];
+    }
+    if(array_diff(array_keys($columns), $real_cols)) {
+      return false;
+    } else {
+      return true;
+    }
+  }
 }
 
 function manipulation_statement($Type, $Form_Data) {
