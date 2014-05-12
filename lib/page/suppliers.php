930dac1af15403dba114802e4904dfe23d8c8634
Rotoloso <ruben.engelmann@web.de>
2014-05-12 12:00:07 +0200

    suppliers list changed

--- lib/page/suppliers.php
+++ lib/page/suppliers.php
@@ -1,17 +1,39 @@
-<h1>Suppliers</h1>
+
 <div class="row">
-	<button >Add</button>
+	<h1>Suppliers</h1>
+	<button type="button" class="btn btn-primary" style="float: right">Add</button>
 </div>
-<div class="table-responsive">
-  <table class="table table-striped" id="suppliers_table">
-	<tr>
-		<td> asd </td>	
-	</tr>
-	
-	<?php
-		//var array = function knifeparty();
+<div class="row">
+	<div class="table-responsive">
+	  <table class="table table-striped" id="suppliers_table">
+		<th>tbl_header1</th>
+		<th>tbl_header1</th>
+		<th>tbl_header1</th>
+		<th></th>  
+		<tr>
+			<td> item 1</td>
+			<td> item 2</td>
+			<td> item 3</td>
+			<td>	
+				<button type="button" class="btn btn-primary btn-xs" style="float: right">Delete</button>
+				<button type="button" class="btn btn-primary btn-xs" style="float: right">Edit</button>
+			</td>	
+		</tr>
+		<tr>
+			<td> item 1</td>
+			<td> item 2</td>
+			<td> item 3</td>
+			<td>	
+				<button type="button" class="btn btn-primary btn-xs" style="float: right">Delete</button>
+				<button type="button" class="btn btn-primary btn-xs" style="float: right">Edit</button>
+			</td>	
+		</tr>
 		
-	?>
-	
-  </table>
+		<?php
+			//var array = function knifeparty();
+			
+		?>
+		
+	  </table>
+	</div>
 </div>
