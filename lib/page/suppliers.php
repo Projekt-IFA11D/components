2a62bbf480899c495f48d120d4a4ef513e086f08
Rotoloso <ruben.engelmann@web.de>
2014-05-12 11:49:57 +0200

    added pages

---
+++ lib/page/suppliers.php
@@ -0,0 +1,17 @@
+<h1>Suppliers</h1>
+<div class="row">
+	<button >Add</button>
+</div>
+<div class="table-responsive">
+  <table class="table table-striped" id="suppliers_table">
+	<tr>
+		<td> asd </td>	
+	</tr>
+	
+	<?php
+		//var array = function knifeparty();
+		
+	?>
+	
+  </table>
+</div>
---
+++ lib/page/users.php
@@ -0,0 +1,10 @@
+<h1>Users</h1>
+<div class="table-responsive">
+  <table class="table table-striped" id="user_table">
+	
+	<?php
+		
+	?>
+	
+  </table>
+</div>
