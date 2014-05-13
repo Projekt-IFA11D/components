20d6e4210c7a41340845a856e2f2cf4b0d7f6090
kpetsch <randombaur@gmail.com>
2014-05-13 14:19:26 +0200

    Complex select handling works now

--- lib/mysql/select_statement.php
+++ lib/mysql/select_statement.php
@@ -35,12 +35,13 @@ INNER JOIN plz_zurodnung AS plz ON (L.l_plz_id=plz.ID)"];
 }
 
 // Handle multiple select statements which need to be returned in a single array
+// Index contains the column name for the where condition
 function complex_select_statement($Table, $Index = 0) {
 
   $Data = array();
   $Sub_Data = array();
   // Still needs the correct select statements for each table
-  $Statements = ["components" => ["SELECT PLACEHOLDER FROM PLACEHOLDER", "SELECT PLACEHOLDER FROM PLACEHOLDER WHERE $Index"]];
+  $Statements = ["components" => ["SELECT * FROM raeume", "SELECT * FROM raeume WHERE $Index"]];
 
   // Escape all special characters inside the string
   if($Index!=0) {
@@ -50,15 +51,16 @@ function complex_select_statement($Table, $Index = 0) {
     }
   }
 
-  $Result = mysql_query($Statements[$Table[0]]);
+  $Result = mysql_query($Statements[$Table][0]);
   while($Data[] = mysql_fetch_assoc($Result));
   array_pop($Data);
   
-  foreach ($Data as $piece) {
-    $Sub_Result = mysql_query($Statements[$Table[1]]);
-    while($Sub_Data[] = mysql_fetch_assoc($Result));
+  foreach ($Data as $key => $piece) {
+    $Sub_Result = mysql_query($Statements[$Table][1]."=".$piece[$Index]);
+    while($Sub_Data[] = mysql_fetch_assoc($Sub_Result));
     array_pop($Sub_Data);
-    $piece[$Table] = $Sub_Data;
+    $Data[$key][$Table] = $Sub_Data;
+    unset($Sub_Data);
   }
 
   mysql_close();
@@ -69,6 +71,7 @@ function complex_select_statement($Table, $Index = 0) {
 // Replace NULL values with visual empty signs
 function nice_empty_values($Data) {
   
+  $nice_Data = array();
   foreach ($Data as $arr_Data) {
     foreach ($arr_Data as $key => $raw_Data) {
       if($raw_Data==NULL && !is_array($raw_Data)) {
