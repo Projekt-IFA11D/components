07b4fed57b1878224b4a12b20e4bccd217cff24a
kpetsch <randombaur@gmail.com>
2014-05-13 13:49:32 +0200

    First version of complex select statement handler

--- lib/mysql/select_statement.php
+++ lib/mysql/select_statement.php
@@ -2,7 +2,6 @@
 /* 
  * Handler for select statements
  * @author = Kilian Petsch
- * @license = 
  * @date = 2014-05-12
 
 */
@@ -13,6 +12,11 @@ function select_statement($Table, $Index = 0) {
   
   $Data = array();
 
+  // Still needs the correct select statements for each table
+  $Statements=["rooms" => "SELECT r_nr, r_bezeichnung, r_notiz FROM raeume",
+			   "suppliers" => "SELECT L.l_firmenname, L.l_strasse, L.l_tel, L.l_mobil, L.l_fax, L.l_email, plz.PLZ, plz.Ort FROM lieferant AS L 
+INNER JOIN plz_zurodnung AS plz ON (L.l_plz_id=plz.ID)"];
+
   // Escape all special characters inside the string
   if($Index!=0) {
     $safe_Index = array();
@@ -20,11 +24,7 @@ function select_statement($Table, $Index = 0) {
       $safe_Index[] = sql_quote($unsafe_index);
     }
   }
-  // Still needs the correct select statements for each table
-  $Statements=["rooms" => "SELECT r_nr, r_bezeichnung, r_notiz FROM raeume",
-			   "suppliers" => "SELECT L.l_firmenname, L.l_strasse, L.l_tel, L.l_mobil, L.l_fax, L.l_email, plz.PLZ, plz.Ort FROM lieferant AS L 
-INNER JOIN plz_zurodnung AS plz ON (L.l_plz_id=plz.ID)"];
-  
+
   $Result = mysql_query($Statements[$Table]);
   while($Data[]=mysql_fetch_assoc($Result));
   // Need to delete last element of the array as fetch_assoc writes its failure into the array
@@ -37,15 +37,41 @@ INNER JOIN plz_zurodnung AS plz ON (L.l_plz_id=plz.ID)"];
 // Handle multiple select statements which need to be returned in a single array
 function complex_select_statement($Table, $Index = 0) {
 
+  $Data = array();
+  $Sub_Data = array();
+  // Still needs the correct select statements for each table
+  $Statements = ["components" => ["SELECT PLACEHOLDER FROM PLACEHOLDER", "SELECT PLACEHOLDER FROM PLACEHOLDER WHERE $Index"]];
 
+  // Escape all special characters inside the string
+  if($Index!=0) {
+    $safe_Index = array();
+    foreach($Index as $unsafe_index) {
+      $safe_Index[] = sql_quote($unsafe_index);
+    }
+  }
+
+  $Result = mysql_query($Statements[$Table[0]]);
+  while($Data[] = mysql_fetch_assoc($Result));
+  array_pop($Data);
+  
+  foreach ($Data as $piece) {
+    $Sub_Result = mysql_query($Statements[$Table[1]]);
+    while($Sub_Data[] = mysql_fetch_assoc($Result));
+    array_pop($Sub_Data);
+    $piece[$Table] = $Sub_Data;
+  }
+
+  mysql_close();
+  return nice_empty_values($Data);
+  
 }
 
-// Replace empty values with visual empty signs
+// Replace NULL values with visual empty signs
 function nice_empty_values($Data) {
   
-  foreach ($Data as $sub_Data) {
-    foreach ($sub_Data as $key => $raw_Data) {
-      if($raw_Data==NULL) {
+  foreach ($Data as $arr_Data) {
+    foreach ($arr_Data as $key => $raw_Data) {
+      if($raw_Data==NULL && !is_array($raw_Data)) {
 	$tmp[$key]="-";
       } else {
 	$tmp[$key]=$raw_Data;
