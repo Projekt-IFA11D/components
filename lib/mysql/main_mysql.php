48a49cf196fa9384648dc1a2e2acac9d03ecd519
Rick Salevsky <rsalevsky@suse.com>
2014-05-13 11:27:23 +0200

    fix config

--- lib/mysql/main_mysql.php
+++ lib/mysql/main_mysql.php
@@ -1,5 +1,5 @@
 <?php
-	mysql_connect(MYSQL_SERVER, MYSQL_USER, MYSQL_PASSWORD);
+	mysql_connect(MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD);
 	mysql_select_db(MYSQL_DATABASE);
 
 	require_once("statement_creator.php");
