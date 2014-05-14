552b821216cb58ca81fa44cce00d6887fd19f400
marcoCBB <marco.stecher@web.de>
2014-05-14 20:32:29 +0200

    edited input names

--- lib/forms/formular_cpu.php
+++ lib/forms/formular_cpu.php
@@ -9,11 +9,11 @@
 <form role="form" type="text">
     <div class="form-group">
         <label for="cpu_name">Interne Bezeichnung/Name:</label>
-        <input type="text" id="cpu_name" class="form-control" name="cpu_name" style="width:300px;">
+        <input type="text" id="cpu_attr_name" class="form-control" name="cpu_name" style="width:300px;">
     </div>
     <div class="form-group">
         <label for="cpu_sockel">Groesse:</label>
-        <input type="text" id="cpu_sockel" class="form-control" name="cpu_sockel" style="width:300px;">
+        <input type="text" id="cpu_sockel" class="form-control" name="cpu_attr_sockel" style="width:300px;">
     </div>
 </form>
 
--- lib/forms/formular_mainboard.php
+++ lib/forms/formular_mainboard.php
@@ -9,44 +9,44 @@
 <form role="form" type="text">
     <div class="form-group">
         <label for="mb_name">Interne Bezeichnung/Name:</label>
-        <input type="text" id="mb_name" class="form-control" name="mb_name" style="width:300px;">
+        <input type="text" id="mainboard_attr_name" class="form-control" name="mainboard_attr__name" style="width:300px;">
     </div>
     <div class="form-group">
         <label for="mb_sockel">Groesse:</label>
-        <input type="text" id="mb_sockel" class="form-control" name="mb_sockel" style="width:300px;">
+        <input type="text" id="mb_sockel" class="form-control" name="mainboard_attr__sockel" style="width:300px;">
     </div>
     <div class="form-group">
         <label for="mb_ram_typ">Ram-Typ</label>
-        <input type="text" id="mb_ram_typ" class="form-control" name="mb_ram_typ" style="width:300px;">
+        <input type="text" id="mb_ram_typ" class="form-control" name="mainboard_attr__ram_typ" style="width:300px;">
     </div>
     <div class="form-group">
         <label for="mb_ram_max">Ram maximal moeglich</label>
-        <input type="text" id="mb_ram_max" class="form-control" name="mb_ram_max" style="width:300px;">
+        <input type="text" id="mb_ram_max" class="form-control" name="mainboard_attr__ram_max" style="width:300px;">
     </div>
     <div class="form-group">
         <label for="mb_baenke_anzahl">Anzahl Baenke:</label>
-        <input type="text" id="mb_baenke_anzahl" class="form-control" name="mb_baenke_anzahl" style="width:300px;">
+        <input type="text" id="mb_baenke_anzahl" class="form-control" name="mainboard_attr__baenke_anzahl" style="width:300px;">
     </div>
     <div class="form-group">
         <label for="mb_stecker_netz">Steckertyp zum Netzteil: </label>
-        <input type="text" id="mb_stecker_netz" class="form-control" name="mb_stecker_netz" style="width:300px;">
+        <input type="text" id="mb_stecker_netz" class="form-control" name="mainboard_attr__stecker_netz" style="width:300px;">
     </div>
     <div class="form-group">
         <label for="mb_stecker_cpu">Steckertyp zum CPU: </label>
-        <input type="text" id="mb_stecker_cpu" class="form-control" name="mb_stecker_cpu" style="width:300px;">
+        <input type="text" id="mb_stecker_cpu" class="form-control" name="mainboard_attr__stecker_cpu" style="width:300px;">
     </div>
     <label for="name">Onboard-Funktionalitaet: </label>
     <div class="checkbox">
-       <label><input type="checkbox" id="mb_grafik" name="mb_grafik" value=""> Grafik</label>
+       <label><input type="checkbox" id="mb_grafik" name="mainboard_attr__grafik" value=""> Grafik</label>
     </div>
     <div class="checkbox">
-       <label><input type="checkbox" id="mb_nic" name="mb_nic" value="mb_nic"> NIC</label>
+       <label><input type="checkbox" id="mb_nic" name="mainboard_attr__nic" value="mb_nic"> NIC</label>
     </div>
     <div class="checkbox">
-       <label><input type="checkbox" id="mb_wol" name="mb_wol" value="mb_wol"> WakeOnLAN</label>
+       <label><input type="checkbox" id="mb_wol" name="mainboard_attr__wol" value="mb_wol"> WakeOnLAN</label>
     </div>
     <div class="checkbox">
-       <label><input type="checkbox" id="mb_raidctrl" name="mb_raidctrl" value="mb_raidctrl"> Raidcontroller</label>
+       <label><input type="checkbox" id="mb_raidctrl" name="mainboard_attr__raidctrl" value="mb_raidctrl"> Raidcontroller</label>
     </div>
     <label for="name">Interne Schnittstellen: </label>
     <div class="checkbox">
@@ -56,7 +56,7 @@
        <label><input type="checkbox" id="mb_int_if" value="mb_int_if_sas"> SAS</label>
     </div>
     <div class="form-group">
-        <label for="">Sonstiges: <input type="text" class="form-control" name="mb_int_if_sonstig" style="width:300px;">
+        <label for="">Sonstiges: <input type="text" class="form-control" name="mainboard_attr__int_if_sonstig" style="width:300px;">
     </div>
     <label for="name">Externe Schnittstellen: </label>
     <div class="checkbox">
@@ -66,7 +66,7 @@
        <label><input type="checkbox" id="mb_ext_if" value="mb_ext_if_usb_3"> SAS</label>
     </div>
     <div class="form-group">
-        <label for="">Sonstiges: <input type="text" name="mb_extern_if_sonstig" style="width:300px;">
+        <label for="">Sonstiges: <input type="text" name="mainboard_attr__extern_if_sonstig" style="width:300px;">
     </div>
 </form>
 
