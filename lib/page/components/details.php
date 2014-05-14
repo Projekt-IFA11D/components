3680135f1c785df3b9b27bb2f8349c1e4344e91c
nwalter <nwalter@suse.de>
2014-05-14 15:05:27 +0200

    added details.php skript

--- lib/page/components/details.php
+++ lib/page/components/details.php
@@ -6,7 +6,36 @@
 
 <?php
 
-	//print_r($_GET["component"]);
+	print_r($_GET["component"]);
+	
+	//1-8 & 13-25
+
+	if(!empty ($_GET["component"]))
+	{
+		switch $_GET["component"]
+		{
+			case 1:
+			form_bundle_pc.php
+			break;
+
+			case 2:
+			formular_ram.php
+			break;
+
+			case 3:
+			formular_cpu.php
+			break;
+
+			case 4:
+			formular_mainboard.php
+			break;
+		}
+	}
+
+?>
+			
+
+
 
 
 ?>
