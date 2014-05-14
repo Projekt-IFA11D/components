ff63318590b47efea618c336c5402342d98892fe
Rick Salevsky <rsalevsky@suse.com>
2014-05-14 18:58:09 +0200

    fix dashboard

--- lib/page/dashboard/main.php
+++ lib/page/dashboard/main.php
@@ -1,2 +1,2 @@
 <h1 class="page-header">Dashboard</h1>
-<img src="https://s3.amazonaws.com/awesome_screenshot/4712068?AWSAccessKeyId=0R7FMW7AXRVCYMAPTPR2&Expires=1400086483&Signature=k4WtFTFxCjP4fOAxa%2FYUrYDVwvc%3D"></img>
+<img src="assets/dashboard.png"></img>
