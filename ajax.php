b5d6ff922462a66c308b7bac45c6d992811130cd
marcoCBB <marco.stecher@web.de>
2014-05-14 11:47:27 +0200

    edited wizzard section

--- ajax.php
+++ ajax.php
@@ -10,24 +10,74 @@
 		  require_once('lib/page/'.$page.'/main.php');
 		}
 	}
-	elseif (isset($_GET['wizzard']))
+elseif (isset($_GET['wizzard']))
 	{
+		
+		
+		
 		$wiz = $_GET['wizzard'];
 		$type = $_GET['type'];
 		if ($wiz == 'bundle')
 		{
+			
+			echo "<form id='compForm'>";
+			
+			echo "<input type='hidden' id='type' value='".$type."'>";
 			if (file_exists('lib/forms/form_bundle_'.$type.'.php'))
 			{
 				require_once('lib/forms/form_bundle_'.$type.'.php');
 			}
+			
+			
+			echo "<br>";
+			
+			echo "</form>";
+			echo "<input type='button' id='bundle_next' onclick='bundle_next()' value='Weiter'>";
+	
+			
+		}
+		elseif($wiz == "save")
+		{
+			order_wizzard_save();
 		}
 		else
 		{
-			if (file_exists('lib/forms/formular_'.$type.'.php'))
+			
+			
+			echo "<form name='compForm'>";
+			echo "Menge: <input type='text' name='anzahl' value='1'>";
+			if(isset($_POST['form']))
 			{
-				require_once('lib/forms/formular_'.$type.'.php');
+				/*
+				echo"<pre>";
+					print_r($_POST);
+				echo"</pre>";*/
+				foreach($_POST['form'] AS $val)
+				{
+					require_once('lib/forms/formular_'.$val.'.php');
+				}
 			}
+			else
+			{
+				if (file_exists('lib/forms/formular_'.$type.'.php'))
+				{
+					require_once('lib/forms/formular_'.$type.'.php');
+				}	
+			}
+			require_once('lib/forms/formular_allgemein.php');
+			
+			echo "<br>";
+			echo "<input type='submit' name='send_comps' value='Anlegen'>";
+			echo "</form>";
+			
 		}
+		
+		
+		
+		
+		
+		
+		
 	}
 	elseif (isset($_GET['edit_room']))
 	{
