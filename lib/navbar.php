82df6ca1ff5391cc51cbb00e04effc05e9ae0ce7
Rick Salevsky <rsalevsky@suse.com>
2014-05-14 21:10:37 +0200

    fix search bootstrap

--- lib/navbar.php
+++ lib/navbar.php
@@ -1,19 +1,11 @@
 <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
   <div class="container-fluid">
     <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
       <a class="navbar-brand" id="page_title" href="#dashboard"><?php echo(PROJEKT_NAME." v".PROJEKT_VERSION) ?></a>
     </div>
     <div class="col-sm-2 col-md-2 pull-right">
-      <form class="navbar-form" role="search" id="search-input-field">
-        <div class="input-group">
-          <input type="text" class="form-control" placeholder="Quick Search" name="srch-term" id="srch-term">
-        </div>
+	  <form class="navbar-form navbar-right">
+        <input type="text" class="form-control" placeholder="Quick Search" name="srch-term" id="srch-term">
       </form>
     </div>
   </div>
