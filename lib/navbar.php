c275ceea2939f63a54b975b77b6469b4e601889e
MaximilianMeister <mmeister@suse.de>
2014-05-14 13:52:44 +0200

    Adding searchbar to Navbar

--- lib/navbar.php
+++ lib/navbar.php
@@ -9,5 +9,15 @@
 	  </button>
 	  <a class="navbar-brand" href="#dashboard"><?php echo(PROJEKT_NAME." ".PROJEKT_VERSION) ?></a>
 	</div>
+	<div class="col-sm-3 col-md-3 pull-right">
+       		 <form class="navbar-form" role="search" id="search-input-field">
+        		<div class="input-group">
+            			<input type="text" class="form-control" placeholder="Search" name="srch-term" id="srch-term">
+            			<div class="input-group-btn">
+                			<button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
+            			</div>
+        		</div>
+        	</form>
+        </div>
   </div>
 </div>
--- lib/page/components/main.php
+++ lib/page/components/main.php
@@ -6,17 +6,48 @@
 	<div class="col-md-2">
 		<h1 class="page-header">Komponenten</h1>
 	</div>
-	<div class="col-sm-3 col-md-3 pull-right">
-       		 <form class="navbar-form" role="search" id="search-input-field">
-        		<div class="input-group">
-            			<input type="text" class="form-control" placeholder="Search" name="srch-term" id="srch-term">
-            			<div class="input-group-btn">
-                			<button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
-            			</div>
-        		</div>
-        	</form>
-        </div>
 </div>
+
+<div class="table table-responsive">
+	<table class="table table-striped">
+            <tr class="header">
+		<th>Firmenname</th>
+		<th>Raum-Bez</th>
+		<th>Einkaufsdatum</th>
+		<th>Gew&auml;hrleistungsdauer</th>
+		<th>Notiz</th>
+		<th>Hersteller</th>
+		<th>Komponentenart</th>
+		<th></th>
+            </tr>
+		<?php
+			$acquisitions = select_statement("acquisitions");
+			foreach ($acquisitions as $acquisition)
+			{ ?>
+					<tr>
+						<td><?php echo($acquisition['l_firmenname']) ?></td>
+						<td><?php echo($acquisition['r_bezeichnung'])?></td>
+						<td><?php echo($acquisition['k_einkaufsdatum']) ?></td>
+						<td><?php echo($acquisition['k_gewaehrleistungsdauer'])." Jahre"?></td>
+						<td><?php echo($acquisition['k_notiz'])?></td>
+						<td><?php echo($acquisition['k_hersteller'])?></td>
+						<td><?php echo($acquisition['ka_komponentenart'])?></td>
+						<!--<td class="col-md-1 edit_components"><button type="button" class="btn btn-primary btn-xs" data-toggle="modal" data-target=".edit_components_modal" onclick="<?php echo('edit_components($(this), '.$acquisition['k_id'].')') ?>">Editieren</button></td>-->
+						<td class="col-md-1">
+						<button type="button" class="btn btn-primary btn-xs" data-toggle="modal" data-target=".details_room_components_modal"
+							onclick="<?php echo('edit_room_components($(this), '.$room_component['k_id'].')') ?>">Details</button>
+						</td>
+						<td class="col-md-1">
+						<button type="button" class="btn btn-danger btn-xs" data-toggle="modal" data-target=".delete_room_components_modal"
+							onclick="<?php echo('delete_room_components('.$room_component['k_id'].')') ?>">L&ouml;schen</button>
+						</td>
+					</tr>
+			<?php }
+		?>
+	<table>
+</div>
+
+<script src="lib/page/components/script.js"></script>
 <script type="text/javascript">
 $(document).ready(function() {
 	$("#srch-term").keyup(function () {
@@ -49,44 +80,4 @@ $(document).ready(function() {
 	});
 });
 </script>
-
-<div class="table table-responsive">
-	<table class="table table-striped">
-		<th>Firmenname</th>
-		<th>Raum-Bez</th>
-		<th>Einkaufsdatum</th>
-		<th>Gew&auml;hrleistungsdauer</th>
-		<th>Notiz</th>
-		<th>Hersteller</th>
-		<th>Komponentenart</th>
-		<th></th>
-		<?php
-			$acquisitions = select_statement("acquisitions");
-			foreach ($acquisitions as $acquisition)
-			{ ?>
-					<tr>
-						<td><?php echo($acquisition['l_firmenname']) ?></td>
-						<td><?php echo($acquisition['r_bezeichnung'])?></td>
-						<td><?php echo($acquisition['k_einkaufsdatum']) ?></td>
-						<td><?php echo($acquisition['k_gewaehrleistungsdauer'])." Jahre"?></td>
-						<td><?php echo($acquisition['k_notiz'])?></td>
-						<td><?php echo($acquisition['k_hersteller'])?></td>
-						<td><?php echo($acquisition['ka_komponentenart'])?></td>
-						<!--<td class="col-md-1 edit_components"><button type="button" class="btn btn-primary btn-xs" data-toggle="modal" data-target=".edit_components_modal" onclick="<?php echo('edit_components($(this), '.$acquisition['k_id'].')') ?>">Editieren</button></td>-->
-						<td class="col-md-1">
-						<button type="button" class="btn btn-primary btn-xs" data-toggle="modal" data-target=".details_room_components_modal"
-							onclick="<?php echo('edit_room_components($(this), '.$room_component['k_id'].')') ?>">Details</button>
-						</td>
-						<td class="col-md-1">
-						<button type="button" class="btn btn-danger btn-xs" data-toggle="modal" data-target=".delete_room_components_modal"
-							onclick="<?php echo('delete_room_components('.$room_component['k_id'].')') ?>">L&ouml;schen</button>
-						</td>
-					</tr>
-			<?php }
-		?>
-	<table>
-</div>
-
-<script src="lib/page/components/script.js"></script>
-
 
