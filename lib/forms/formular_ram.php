3bb578d875240f5f7ddc2d9f4cc0136b8c2ffe9a
marcoCBB <marco.stecher@web.de>
2014-05-14 21:30:31 +0200

    added "attr" flag to input names

--- lib/forms/formular_ram.php
+++ lib/forms/formular_ram.php
@@ -2,14 +2,14 @@
 <form role="form" type="text">
     <div class="form-group">
         <label for="ram_name">Interne Bezeichnung/Name:</label>
-        <input type="text" id="ram_name" class="form-control" name="ram_name" style="width:300px;">
+        <input type="text" id="ram_name" class="form-control" name="ram_attr_name" style="width:300px;">
     </div>
     <div class="form-group">
         <label for="ram_groesse">Groesse:</label>
-        <input type="text" id="ram_groesse" class="form-control" name="ram_groesse" style="width:300px;">
+        <input type="text" id="ram_groesse" class="form-control" name="ram_attr_groesse" style="width:300px;">
     </div>
     <div class="form-group">
         <label for="ram_taktfrequenz">Taktfrequenz:</label>
-        <input type="text" id="ram_taktfrequenz" class="form-control" name="ram_taktfrequenz" style="width:300px;">
+        <input type="text" id="ram_taktfrequenz" class="form-control" name="ram_attr_taktfrequenz" style="width:300px;">
     </div>
 </form>
