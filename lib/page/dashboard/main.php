f630a4537ef48f6622422598d7d52d6db3fdf676
MaximilianMeister <mmeister@suse.de>
2014-05-15 07:41:57 +0200

    random shice

--- lib/page/dashboard/main.php
+++ lib/page/dashboard/main.php
@@ -1,2 +1,8 @@
 <h1 class="page-header">Dashboard</h1>
-<img src="assets/dashboard.png"></img>
+<?php
+$coders = ["Werni","Soulofdestiny","WeizenFaust","randombauer","MaximilianMeister","nwalter1","dreschmn","marcoCBB","Rotoloso","GerritHanno","ProofofPayment","iMontouch","3ngl3r"];
+
+$victim = $coders[array_rand($coders)];
+echo "<p>$victim</p>";
+echo "<iframe src='http://osrc.dfm.io/".$victim."' seamless height='1000px' width='100%' class='iframe'></iframe>";
+?>
