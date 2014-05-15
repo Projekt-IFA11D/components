e7e2c15e9b3aa54d532990ca221a60a49a5df0ad
kpetsch <randombaur@gmail.com>
2014-05-15 08:09:47 +0200

    Cleaned up, created dummy for hardcoded manipulation statements

--- lib/mysql/manipulation_statements.php
+++ lib/mysql/manipulation_statements.php
@@ -68,10 +68,6 @@ function manipulation_statement($Type, $Form_Data) {
   }
 
   $Table_Columns = read_column_names($Form_Data);
-  /* DOES NOT WORK
-    if(!check_column_names($Table_Columns)) {
-    // Raise some kind of error
-    } */
   
   $Statements = create_statement($Table_Columns, $Type, [$Index]);
   for($i=0;$i<$Anzahl;$i++) {
@@ -81,59 +77,13 @@ function manipulation_statement($Type, $Form_Data) {
   }
 }
 
-/* DOES NOT WORK WRITE A HARDCODED FUNCTION
-function find_attribution_table($Data) {
-  
-  $Columns = array();
-  $Attr_Tables = array();
-  foreach (array_keys($Data) as $Table) {
-    $SQL = "SELECT
-    `column_name`, 
-    `referenced_table_schema` AS foreign_db, 
-    `referenced_table_name` AS foreign_table, 
-    `referenced_column_name`  AS foreign_column 
-FROM
-    `information_schema`.`KEY_COLUMN_USAGE`
-WHERE
-    `constraint_schema` = SCHEMA()
-AND
-    `table_name` = $Table
-AND
-    `referenced_column_name` IS NOT NULL
-ORDER BY
-    `column_name`";
-    if(mysql_query($SQL)=="") {
-      $Attr_Tables[] = $Table;
-    }
-  }
-  return $Attr_Tables;
+function complex_manipulation_statement($Index, $Form_Data) {
+
+  // Array of statements
+  $Statements = ["A" => "B"];
+  mysql_query($Statements[$Index]];
+
 }
-
-
-function attribution_manipulation_statement($Type, $Form_Data) {
-
-  // Unsetting the timestamp as we don't need it here
-  if(isset($Form_Data["_"])) {
-    unset($Form_Data["_"]);
-  }
-  if(isset($Form_Data["Anzahl"]) && $Form_Data["Anzahl"] > 0) {
-    $Anzahl = $Form_Data["Anzahl"];
-    unset($Form_Data["Anzahl"]);
-  } else {
-    $Anzahl = 1;
-    unset($Form_Data["Anzahl"]);
-  }
-  
-  $Table_Columns = read_column_names($Form_Data);
-  $Missing_Vals = check_column_names($Table_Columns);
-
-  $Attr_Tables = find_attribution_table($Table_Columns);
-  $Attr_Values = array();
-  mysql_query("SELECT $Missing_Vals FROM $Attr_Tables WHERE ")
-    
-  }
-  
-  } */
 
 function not_really_delete($Index) {
   mysql_query("UPDATE komponenten SET raeume_r_id=8 WHERE $Index");
