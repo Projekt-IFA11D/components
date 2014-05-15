ab240c2b7577dab7cf7d9709b68aaffe2d4cf959
Rick Salevsky <rsalevsky@suse.com>
2014-05-15 20:03:45 +0200

    first version of the dashboard

--- lib/dashboard/main.php
+++ lib/dashboard/main.php
@@ -1,7 +1,30 @@
-<h1 class="page-header">Dashboard</h1>
 <?php
-$coders = ["Werni","Soulofdestiny","WeizenFaust","randombauer","MaximilianMeister","nwalter1","dreschmn","marcoCBB","Rotoloso","GerritHanno","ProofofPayment","iMontouch","3ngl3r"];
-
-$victim = $coders[array_rand($coders)];
-//echo "<iframe src='http://osrc.dfm.io/".$victim."' seamless height='1000px' width='100%' class='iframe'></iframe>";
+	$time = date("H:i",time());
+	$rooms = count(select_statement("rooms"));
+	$suppliers = count(select_statement("suppliers"));
+	$components = count(select_statement('components'));
 ?>
+<h1 class="page-header">Dashboard</h1>
+
+<p class="lead">
+	Wilkommen, aktuell können wir folgende Statistik vorweisen:
+</p>
+
+<div class="table table-responsive">
+	<table class="table table-striped">
+		<th>Art</th>
+		<th>Anzahl</th>
+			<tr>
+				<td>R&auml;ume</td>
+				<td><?php echo($rooms) ?></td>
+			</tr>
+			<tr>
+				<td>Lieferranten</td>
+				<td><?php echo($suppliers) ?></td>
+			</tr>
+			<tr>
+				<td>Komboneneten</td>
+				<td><?php echo($components) ?></td>
+			</tr>
+	<table>
+</div>
