c39621dd28d770ae08e1ce5549eb409d6e05d8cb
marcoCBB <marco.stecher@web.de>
2014-05-15 11:43:11 +0200

    form names

--- lib/forms/formular_accesspoints.php
+++ lib/forms/formular_accesspoints.php
@@ -25,12 +25,12 @@ echo "<table border = '1' cellpadding = '0' cellspacing='4'>
 
 		<tr>
 		<td align='right'>Interne Bezeichnung/Name: </td>
-		<td><input name='accesspoint_attr_interne bezeichnung' type='text' size='30'></td>
+		<td><input name='accesspoints_attr_interne bezeichnung' type='text' size='30'></td>
 		</tr>
 
 		<tr>
 		<td align='right'>IP: </td>
-		<td><input name='accesspoint_attr_ip-adresse' type='text' size='30'></td>
+		<td><input name='accesspoints_attr_ip-adresse' type='text' size='30'></td>
 		</tr>
 
 	  </table>";
