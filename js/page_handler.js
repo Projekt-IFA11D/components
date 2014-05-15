2ccc5b622d51a097e4c1b11deb4933ae5895d4d1
MaximilianMeister <mmeister@suse.de>
2014-05-15 08:06:48 +0200

    Move quick search to pagehandler.js

--- js/page_handler.js
+++ js/page_handler.js
@@ -13,3 +13,34 @@ $( "a[href^=#]" ).on('click', function(e) {
 	});
 	e.preventDefault();
 });
+
+// Quick Search
+$(document).ready(function() {
+        $("#srch-term").keyup(function () {
+            var data = this.value.split(" ");
+            var jo = $(".table").find("tr");
+            if (this.value == "") {
+                      jo.show();
+                      return;
+            }
+      $(".table tr:not(:first-child)").hide(); 
+
+      jo.filter(function (i, v) {
+                      var $t = $(this);
+                      for (var d = 0; d < data.length; ++d) {
+                          if ($t.is(":contains('" + data[d] + "')")) {
+                                return true;
+                          }
+                }
+                return false;
+            }).show();
+        }).focus(function () {
+            this.value = "";
+            $(this).css({
+               "color": "black"
+            });
+            $(this).unbind('focus');
+        }).css({
+           "color": "#C0C0C0"
+        });
+});
