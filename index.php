a5211b0bd7672281a1641460b376f8ca2f4a2790
MaximilianMeister <mmeister@suse.de>
2014-05-15 13:10:07 +0200

    local jquery

--- index.php
+++ index.php
@@ -46,7 +46,7 @@
     <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script src="js/jquery-1.11.1.min.js"></script>
     <script src="assets/js/docs.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
     <script src="js/page_handler.js"></script>
