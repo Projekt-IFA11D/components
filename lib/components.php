54541d8e79b80e994e713ac1aa49695f942f8c29
Rotoloso <ruben.engelmann@web.de>
2014-05-15 09:59:39 +0200

    fixed components requires

--- lib/components.php
+++ lib/components.php
@@ -1,8 +1,6 @@
 <?php
 	require_once('components/main.php');
-	var_dump($_GET);
 	require_once('components/delete.php');
-	require_once('components/details.php');
 ?>
 
 <script src="lib/components/script.js"></script>
