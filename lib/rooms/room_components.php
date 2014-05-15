729f12227f8f84b0b8e02ace370ae5cc1c6eb43c
Rick Salevsky <rsalevsky@suse.com>
2014-05-15 19:03:18 +0200

    only room components

--- lib/rooms/room_components.php
+++ lib/rooms/room_components.php
@@ -13,7 +13,7 @@
 			<th></th>
 			<th></th>
 		<?php
-			$components = complex_select_statement('main_components');
+			$components = complex_select_statement('main_components', $_GET['room_component']);
 			foreach ($components as $component)
 			{ ?>
 					<tr>
