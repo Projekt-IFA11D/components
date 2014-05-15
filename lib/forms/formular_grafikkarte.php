4119fbe7c6de7419f443f31506221070abae2f34
marcoCBB <marco.stecher@web.de>
2014-05-15 10:29:23 +0200

    fixed input names

--- lib/forms/formular_grafikkarte.php
+++ lib/forms/formular_grafikkarte.php
@@ -9,15 +9,14 @@
 <form role="form" type="text">
     <div class="form-group">
         <label for="gk_name">Interne Bezeichnung/Name:</label>
-        <input type="text" name="grafikkarte_attr_intern bezeichnung" class="form-control" id="gk_name" style="width:300px;">
+        <input type="text" name="grafikkarte_attr_interne bezeichnung" class="form-control" id="gk_name" style="width:300px;">
     </div>
     <div class="form-group">
         <label for="gk_intern_if">Interne Schnittstelle:</label>
         <input type="text" name="grafikkarte_attr_schnittstelle (intern)" class="form-control" id="gk_intern_if" style="width:300px;">
     </div>
     <div class="form-group">
-        <label for="gk_speicher">Speicher:</label>
+        <label for="gk_speicher">Speichergröße:</label>
         <input type="text" name="grafikkarte_attr_speicherkapazität" class="form-control" id="gk_speicher" style="width:300px;">
     </div>
 </form>
-
