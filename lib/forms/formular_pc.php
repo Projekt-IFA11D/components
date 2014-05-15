b22e8b94918a526eb1019acacd0129809b372fa2
marcoCBB <marco.stecher@web.de>
2014-05-15 22:54:16 +0200

    bootstrapse

--- lib/forms/formular_pc.php
+++ lib/forms/formular_pc.php
@@ -16,37 +16,37 @@
 /*|													 |*/
 /*| Beschreibung:									 |*/
 /*|	=============									 |*/
-/*|	Formular fuer die Komponente 'PC'		 |*/
+/*|	Formular fuer die Komponente 'PC'	          	 |*/
 /*|													 |*/
 /*+--------------------------------------------------+*/
 
 echo "<h1>PC</h1><br><br>";
-echo "<table border = '1' cellpadding = '0' cellspacing='4'>
+echo "<form role='form' type='text'>";
+echo "  <div class=\"form-group\">
+		<label for=\"pc_name\">Interne Bezeichnung/Name:</label>
+		<input name='komplettsystem_attr_interne bezeichnung' id=\"pc_name\" class=\"form-control\" type='text' size='30'>
+		</div>
 		
-		<tr>
-		<td align='right'>Interne Bezeichnung/Name: </td>
-		<td><input name='komplettsystem_attr_interne bezeichnung' type='text' size='30'></td>
-		</tr>
+		<div class=\"form-group\">
+		<label for=\"pc_ip\">IP des Geraetes:</label> 
+		<input name='komplettsystem_attr_ip-adresse[]' id=\"pc_ip\" class=\"form-control\" type='text' size='30'>
+		</div>
 		
-		<tr>
-		<td align='right'>IP des Geraetes: </td>
-		<td><input name='komplettsystem_attr_ip-adresse[]' type='text' size='30'></td>
-		</tr>
-		
-        <tr>
-		<td align='right'>IP des Geraetes: </td>
-		<td><input name='komplettsystem_attr_ip-adresse[]' type='text' size='30'></td>
-		</tr>
+        <div class=\"form-group\">
+		<label for=\"pc_ip2\">IP des Geraetes: </label>
+		<input name='komplettsystem_attr_ip-adresse[]' id=\"pc_ip\" class=\"form-control\" type='text' size='30'>
+		</div>
         
-		<tr>
-		<td align='right'>Subnetzmaske: </td>
-		<td><input name='komplettsystem_attr_subnetzmaske' type='text' size='30'></td>
-		</tr>
+		<div class=\"form-group\">
+		<label for=\"pc_subnetmask\">Subnetzmaske: </label>
+		<input name='komplettsystem_attr_subnetzmaske' id=\"pc_ip\" class=\"form-control\" type='text' size='30'>
+		</div>
 		
-		<tr>
-		<td align='right'>Gateway: </td>
-		<td><input name='komplettsystem_attr_gateway' type='text' size='30'></td>
-		</tr>
+	    <div class=\"form-group\">
+		<label for=\"pc_gateway\">Gateway: </label>
+		<input name='komplettsystem_attr_gateway' id=\"pc_ip\" class=\"form-control\" type='text' size='30'>
+		</div>
 		
-	  </table>";
+	  
+      </form>";
 ?>
