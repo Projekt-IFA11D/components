a09e770ad6503bba7dcd45c33e67ab08995df7b9
WeizenFaust <weizenfaust@posteo.de>
2014-05-15 09:47:59 +0200

    aquisition paths fixed

--- lib/acquisitions.php
+++ lib/acquisitions.php
@@ -1,1 +1,5 @@
-<?php require_once("acquisitions/main.php"); ?>
+<?php 
+	require_once("acquisitions/main.php"); 
+	require_once("components/delete.php");
+?>
+<script src="lib/components/script.js"></script>
