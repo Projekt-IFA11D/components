3e644007a49a6739ab305b2aa3c39e07a50fd61b
marcoCBB <marco.stecher@web.de>
2014-05-15 08:10:05 +0200

    renamed cpu to prozessor for wizzard handling

---
+++ lib/forms/formular_prozessor.php
@@ -0,0 +1,19 @@
+<!--
+### form for CPU                         ##
+### based on php/html by Max Drescher    ##
+### bootstrapped by Matthias Griessmeier ##
+###
+--> 
+
+<h1>CPU</h1><br><br>
+<form role="form" type="text">
+    <div class="form-group">
+        <label for="cpu_attr_interne bezeichnung">Interne Bezeichnung/Name:</label>
+        <input type="text" id="prozessor_attr_name" class="form-control" name="prozessor_name" style="width:300px;">
+    </div>
+    <div class="form-group">
+        <label for="cpu_attr_sockel">Groesse:</label>
+        <input type="text" id="prozessor_sockel" class="form-control" name="prozessor_attr_sockel" style="width:300px;">
+    </div>
+</form>
+
