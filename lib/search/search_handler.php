486b2deb2c96e67cbe26e6bdca73912be04340eb
Soulofdestiny <mgriessmeier@suse.de>
2014-05-15 14:52:10 +0200

    bootstrapped search result

--- lib/search/search_handler.php
+++ lib/search/search_handler.php
@@ -25,7 +25,7 @@
 /*|	DEFINE VARIABLES								 |*/
 /*+--------------------------------------------------+*/
 
-    var_dump($_GET);
+   // var_dump($_GET);
 	
 	$where_condition = "";
 	$where_condition .= get_where_condition("sel_hersteller" , "k.k_hersteller");
@@ -36,47 +36,53 @@
 	$where_condition .= get_where_condition("sel_notiz" , "k.k_notiz");
 	
 	// DEBUG
-	echo "where:".$where_condition;
+	//echo "where:".$where_condition;
 	$result = array();
 	
 	
 	// Function calls
 	$result = select_statement("search_filter", $where_condition);
-	print_r(select_statement("search_filter", $where_condition));
-	print_r($result);	
+	//print_r(select_statement("search_filter", $where_condition));
+	//print_r($result);	
 	print_result($result);	
-	
+
 	/**
 	 * prints out the restults of the search
 	 * @param $result   array with searched data
 	 */
-	function print_result($result) {
-		echo "<table border = '1'>
-					<tr>
-						<td width='200px'>Firmenname</td>
-						<td width='200px'>Raumbezeichnung</td>
-						<td width='200px'>Einkaufsdatum</td>
-						<td width='200px'>Gewaerhleistungsdauer</td>
-						<td width='200px'>Notiz</td>
-						<td width='200px'>Hersteller</td>
-						<td width='200px'>Komponentenart</td>
-					</tr>";
-					for($i = 0; $i < count($result); $i++) {
-						echo "<tr>";						
-							echo "<td>".$result[$i]['l_firmenname']."</td>";
-							echo "<td>".$result[$i]['r_bezeichnung']."</td>";
-							echo "<td>".$result[$i]['k_einkaufsdatum']."</td>";
-							echo "<td>".$result[$i]['k_gewaehrleistungsdauer']."</td>";
-							echo "<td>".$result[$i]['k_notiz']."</td>";
-							echo "<td>".$result[$i]['k_hersteller']."</td>";
-							echo "<td>".$result[$i]['ka_komponentenart']."</td>";
+    function print_result($result) { ?>
+        <h1>Suchergebnis</h1>
+        <div class="table-responsive">
+                <table class="table table-striped" id="search_result_table">
+                    <th>Firmenname</th>
+                    <th>Raumbezeichnung</th>
+                    <th>Einkaufsdatum</th>
+                    <th>Gew&auml;hrleistungsdauer (Jahre)</th>
+                    <th>Notiz</th>
+                    <th>Hersteller</th>
+                    <th>Komponentenart</th>
+<?php
+             
+                for($i = 0; $i < count($result); $i++) 
+                {
+                
+				    echo "<tr>";						
+						echo "<td>".$result[$i]['l_firmenname']."</td>";
+						echo "<td>".$result[$i]['r_bezeichnung']."</td>";
+						echo "<td>".$result[$i]['k_einkaufsdatum']."</td>";
+						echo "<td>".$result[$i]['k_gewaehrleistungsdauer']."</td>";
+						echo "<td>".$result[$i]['k_notiz']."</td>";
+						echo "<td>".$result[$i]['k_hersteller']."</td>";
+						echo "<td>".$result[$i]['ka_komponentenart']."</td>";
 						
-						echo "</tr>";
-					}
-				echo "</table>";
-	} /** end of print_result */
+                        echo "</tr>";
+                }
+                    /** end of print_result */
+        }?>
+  </table>
+</div>
 	
-	
+<?php	
 	/**
 	 * function creates WHERE conditions
 	 * @param  $select_param   name of the multiple select
@@ -84,16 +90,16 @@
 	 * @return string          WHERE condition
 	 */
 	function get_where_condition($select_param, $name) {
-        echo("<br><br>START:");
-        var_dump($_GET[$select_param]);
-        echo("<br><br>EDNE");
+       // echo("<br><br>START:");
+       // var_dump($_GET[$select_param]);
+        // echo("<br><br>EDNE");
 		global $where_condition;
 		// Delete last added 'AND' if POST is empty
 		if (empty($_GET[$select_param])) {
 			$check = substr( $where_condition, strlen($where_condition)-4, strlen($where_condition) );
 			if ($check == "AND ") {
 				$where_condition = substr ( $where_condition , 0, strlen($where_condition)-4 );
-				echo "where bearbeitet: ".$where_condition."<br>";
+		//		echo "where bearbeitet: ".$where_condition."<br>";
 			}			
  			return "";
 		}
@@ -107,7 +113,7 @@
 		$first = true;		
 		
 		foreach($_GET[$select_param] AS $current) {
-			echo "Current: ".$current."<br>";
+	    //	echo "Current: ".$current."<br>";
 			if($current != "" AND $current != "-") {
 				if(!$first) {
 					$where_query .= " OR ";
@@ -120,7 +126,7 @@
         }		
 		$where_query .= " ) ";
 
-		echo "WHERE:".$where_query."PARAM: ".$select_param."<br><br>";
+		//echo "WHERE:".$where_query."PARAM: ".$select_param."<br><br>";
 
 		return $where_query;
 	} /** end of get_where_condition */
